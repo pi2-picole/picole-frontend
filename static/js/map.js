70869f4c1d68334ff95507f0530b4fffd5f7098e
Ludimila <lbelacruz@gmail.com>
2017-04-18 16:20:43 -0300

    unindo metodos de pegar localização atual e traçar rotas

--- static/js/map.js
+++ static/js/map.js
@@ -1,10 +1,11 @@
-
-
+var posCORRETA = {}
+var saborizze = {lat: -16.254184, lng: -47.958176}
+//inicia o mapa
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
+    zoom: 10,
     center: {lat: -15.793879, lng: -47.882760},
   });
   directionsDisplay.setMap(map);
@@ -14,11 +15,59 @@ function initMap() {
   };
   document.getElementById('start').addEventListener('change', onChangeHandler);
   document.getElementById('end').addEventListener('change', onChangeHandler);
+
+
+  //seta balãozinho com a localização
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            posCORRETA = pos
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Você está aqui!');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+
+       var marker1 = new google.maps.Marker({
+        position: saborizze,
+        map: map,
+        title: 'Saborizze!'
+      });
+
+      var marker2 = new google.maps.Marker({
+        position: document.getElementById('end').value,
+        map: map,
+        title: 'Saborizze!'
+      });
+
+        
 }
 
+//caso não consiga pegar a localização do usuário ou ele se recuse a permitir 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+
+
+//calcula a rota, tem que adicionar os outros tipos de travelMode
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   directionsService.route({
-    origin: document.getElementById('start').value,
+    origin: posCORRETA,
     destination: document.getElementById('end').value,
     travelMode: 'WALKING'
   }, function(response, status) {
