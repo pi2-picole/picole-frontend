b3b8d7db77f655820a1a8412836b39a4875b1479
João Guilherme <joaaogui@gmail.com>
2017-05-15 15:39:54 -0300

    Refatorando storage e requests

--- js/requests.js
+++ js/requests.js
@@ -1,18 +1,10 @@
 $(document).ready(function(){
 
-// jQuery.get('https://picole-pi2.herokuapp.com/popsicles/2', function(data) {
-//       var teste = data.flavor;
-//       document.getElementById("sabor").innerHTML = teste;
-//
-//   });
-
 jQuery.get('https://picole-pi2.herokuapp.com/machines/1', function(data) {
     for(let i=0; i<data.stocks.length; i++){
-
       $('#sabor-'+ i).text(data.stocks[i].popsicle.flavor);
       $('#quantity-'+ i).val(data.stocks[i].amount);
     }
-
   });
 
 })
