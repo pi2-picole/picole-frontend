8044cdd992223884afe49d9f90cb4a83efd1c07a
João Guilherme <joaaogui@gmail.com>
2017-05-18 20:37:13 -0300

    Finalizando login de admin e vendedor

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   jQuery('.header').load('header.html');
   jQuery('.headerAdmin').load('headerAdmin.html');
+  jQuery('.headerVendor').load('headerVendor.html');
   jQuery('.footer').load('footer.html');
 })
