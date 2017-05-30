2e288a42f54d1bc1d033af0123ab728b6e7b002f
João Guilherme <joaaogui@gmail.com>
2017-05-30 13:17:28 -0300

    Adicionando exemplos de post

--- js/requests.js
+++ js/requests.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-jQuery.get('https://picole-pi2.herokuapp.com/machines/1', function(data) {
+  jQuery.get('https://picole-pi2.herokuapp.com/machines/1', function(data) {
     for(let i=0; i<data.stocks.length; i++){
       $('#sabor-'+ i).text(data.stocks[i].popsicle.flavor);
       $('#quantity-'+ i).val(data.stocks[i].amount);
@@ -8,13 +8,28 @@ jQuery.get('https://picole-pi2.herokuapp.com/machines/1', function(data) {
   });
 
   jQuery.post("https://picole-pi2.herokuapp.com/popsicles/" ,
-     	 	{
-            "id": 6,
-            "flavor": "TRYING",
-            "price": "1.50",
-            "is_active": true
-          }
-
-      )
-
+ 	{
+        "id": 6,
+        "flavor": "TRYING",
+        "price": "150",
+        "is_active": true
+      }
+  )
 })
+
+
+function login(){
+
+  var username = $('#username').val();
+  var password = $('#password').val();
+
+  jQuery.post("https://picole-pi2.herokuapp.com/popsicles/" ,
+  {
+    "password": password,
+    "username": username,
+
+    
+  })
+
+  console.log("Username : " + username + password);
+}
