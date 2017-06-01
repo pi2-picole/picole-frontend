05b10170983f6942b9d3871ff43f0df097f43294
João Guilherme <joaaogui@gmail.com>
2017-06-01 16:56:41 -0300

    Adicionando combobox para machines em storage

--- js/requests.js
+++ js/requests.js
@@ -6,11 +6,30 @@ $(document).ready(function(){
       console.log(data.stocks[i].popsicle)
       $('#quantity-'+ i).val(data.stocks[i].amount);
     }
+  });
+
+    jQuery.get('https://picole-pi2.herokuapp.com/machines/', function(data) {
+
+
+      var sel = document.getElementById('comboMachines');
+      var opt = null;
+
+      for(i = 0; i<data.length; i++) { 
+
+        opt = document.createElement('option');
+        opt.value = data[i].id;
+        opt.innerHTML = data[i].id;
+        sel.appendChild(opt);
+      }
 
   });
+
 })
 
-function postStorage(){
+function patchStorage(){
+
+    var idMaquina = $('#comboMachines').val();
+    console.log(idMaquina);
 
 }
 
