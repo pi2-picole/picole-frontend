e2cffbc67d968c8c6070fa4a5be4969576e01e03
Ludimila <lbelacruz@gmail.com>
2017-06-01 19:25:07 -0300

    Adicionando tempo de expiracao

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -1,73 +1,33 @@
-(function($) {
-	$.fn.countdown = function(options, callback) {
+// Set the date we're counting down to
+var date = new Date();
+var countDownDate = new Date();
+countDownDate.setHours (date.getHours() + 8 );
 
-		//custom 'this' selector
-		thisEl = $(this);
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-		//array of custom settings
-		var settings = { 
-			'date': null,
-			'format': null
-		};
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("hour").innerHTML = hours
 
-		//append the settings array to options
-		if(options) {
-			$.extend(settings, options);
-		}
-		
-		//main countdown function
-		function countdown_proc() {
-			
-			eventDate = Date.parse(settings['date']) / 1000;
-			currentDate = Math.floor($.now() / 1000);
-			
-			if(eventDate <= currentDate) {
-				callback.call(this);
-				clearInterval(interval);
-			}
-			
-			seconds = eventDate - currentDate;
-			
-			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
-			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
-			
-			hours = Math.floor(seconds / (60 * 60));
-			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
-			
-			minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
-			
-			//conditional Ss
-			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
-			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-			
-			//logic for the two_digits ON setting
-			if(settings['format'] == "on") {
-				days = (String(days).length >= 2) ? days : "0" + days;
-				hours = (String(hours).length >= 2) ? hours : "0" + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
-			}
-			
-			//update the countdown's html values.
-			if(!isNaN(eventDate)) {
-				thisEl.find(".days").text(days);
-				thisEl.find(".hours").text(hours);
-				thisEl.find(".minutes").text(minutes);
-				thisEl.find(".seconds").text(seconds);
-			} else { 
-				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
-				clearInterval(interval); 
-			}
-		}
-		
-		//run the function
-		countdown_proc();
-		
-		//loop the function
-		interval = setInterval(countdown_proc, 1000);
-		
-	}
-}) (jQuery);
+    // Output the result in an element with id="demo"
+    document.getElementById("minutes").innerHTML = minutes 
+
+		// Output the result in an element with id="demo"
+    document.getElementById("seconds").innerHTML =  seconds
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("expire").innerHTML = "EXPIRADO";
+    }
+}, 1000);
