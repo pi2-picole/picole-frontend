64aabc7b451e40e2b4a76f049b0635643384aa14
João Guilherme <joaaogui@gmail.com>
2017-06-12 14:40:19 -0300

    Corrigindo erros de formatação e consoles desnecessários

--- js/session.js
+++ js/session.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-    console.log("Entrou");
     if(Cookies.get('token') == null){
-       window.location.replace("/login.html"); 
+       window.location.replace("/login.html");
        alert('Faça login!');  //
     }
     else{
-        console.log("Null")
     }
 })
