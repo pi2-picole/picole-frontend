841c27f49f2f0e0dbc3f5b96aaf85a961cc086f0
Ludimila <lbelacruz@gmail.com>
2017-06-22 20:54:13 -0300

    Finalizando total e preco

---
+++ js/payment.js
@@ -0,0 +1,33 @@
+function calcTotal(){
+  var  flavor0 = document.getElementById('flavor0').value
+  var  flavor1 = document.getElementById('flavor0').value
+  var  flavor2 = document.getElementById('flavor0').value
+
+
+}
+
+
+
+
+function payment() {
+
+  {
+   "MerchantOrderId":"2014111703",
+   "Customer":{
+      "Name":"Comprador crédito simples"
+   },
+   "Payment":{
+     "Type":"CreditCard",
+     "Amount":15700,
+     "Installments":1,
+     "SoftDescriptor":"123456789ABCD",
+     "CreditCard":{
+         "CardNumber":"1234123412341231",
+         "Holder":"Teste Holder",
+         "ExpirationDate":"12/2030",
+         "SecurityCode":"123",
+         "Brand":"Visa"
+     }
+   }
+}
+}
