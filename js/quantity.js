ba240fccb350613f50df65ded4842415fcb14b0e
Parley Martins <parleypachecomartins@gmail.com>
2017-06-23 19:35:27 -0300

    Requisicao pro backend funcionando corretamente

--- js/quantity.js
+++ js/quantity.js
@@ -10,12 +10,12 @@ var totalPrice = 0
         var quantity = parseInt($('#quantity0').val())
         price = $('#flavor0').text()
 
-        var flavor0 = $('#flavor0')   
-            $('#quantity0').val(quantity + 1) 
+        var flavor0 = $('#flavor0')
+            $('#quantity0').val(quantity + 1)
             total+= 1
             totalPrice += parseValue(price)
             $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
-    }) 
+    })
      $('#button0-minus').click(function(e){
         // Stop acting like a button
         e.preventDefault()
@@ -30,17 +30,17 @@ var totalPrice = 0
                 $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
             }
     })
-    $('#button1-plus').click(function(e){     
+    $('#button1-plus').click(function(e){
         // Stop acting like a button
         e.preventDefault()
         // Get the field name
         var quantity = parseInt($('#quantity1').val())
         price = $('#flavor1').text()
 
-            $('#quantity1').val(quantity + 1)   
+            $('#quantity1').val(quantity + 1)
             total += 1
-            totalPrice += parseValue(price)   
-            $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))  
+            totalPrice += parseValue(price)
+            $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
     })
      $('#button1-minus').click(function(e){
         // Stop acting like a button
@@ -52,22 +52,22 @@ var totalPrice = 0
             if(quantity>0){
                 $('#quantity1').val(quantity - 1)
                 total += -1
-                totalPrice -= parseValue(price)               
-                $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))             
+                totalPrice -= parseValue(price)
+                $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
             }
     })
-    $('#button2-plus').click(function(e){     
+    $('#button2-plus').click(function(e){
         // Stop acting like a button
         e.preventDefault()
         // Get the field name
         var quantity = parseInt($('#quantity2').val())
         price = $('#flavor2').text()
-                    
-            $('#quantity2').val(quantity + 1)  
+
+            $('#quantity2').val(quantity + 1)
             total += 1
-            totalPrice += parseValue(price)          
-            $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice)) 
-      
+            totalPrice += parseValue(price)
+            $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
+
     })
      $('#button2-minus').click(function(e){
         // Stop acting like a button
@@ -79,20 +79,20 @@ var totalPrice = 0
             if(quantity>0){
                 $('#quantity2').val(quantity - 1)
                 total += -1
-                totalPrice -= parseValue(price)              
-                $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice)) 
+                totalPrice -= parseValue(price)
+                $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
             }
     })
-    $('#button3-plus').click(function(e){     
+    $('#button3-plus').click(function(e){
         // Stop acting like a button
         e.preventDefault()
         // Get the field name
         var quantity = parseInt($('#quantity3').val())
         price = $('#flavor3').text()
-                    
-            $('#quantity3').val(quantity + 1) 
+
+            $('#quantity3').val(quantity + 1)
             total += 1
-            totalPrice += parseValue(price)           
+            totalPrice += parseValue(price)
             $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
 
     })
@@ -106,7 +106,7 @@ var totalPrice = 0
             if(quantity>0){
                 $('#quantity3').val(quantity - 1)
                 total += -1
-                totalPrice -= parseValue(price)               
+                totalPrice -= parseValue(price)
                 $('#totalPrice').text('Total R$:'+ formatPrice(totalPrice))
             }
     })
@@ -114,64 +114,63 @@ var totalPrice = 0
 
 function parseValue(price){
     price = price.split("$")
-    price = parseFloat(price[1].replace(/,/g, ''))  
+    price = parseFloat(price[1].replace(/,/g, ''))
     return price
 }
 
 function formatPrice(price){
-  price = price.toString()  
-  price = price.replace(/(\d{1})(\d{1,2})$/,"$1,$2") 
+  price = price.toString()
+  price = price.replace(/(\d{1})(\d{1,2})$/,"$1,$2")
   return price
 }
 
 
 function pay(){
-    
+  console.log(totalPrice)
+
     var firstNumber = $('#firstNumber').val()
     var secondNumber = $('#secondNumber').val()
     var thirdNumber = $('#thirdNumber').val()
     var fourthNumber = $('#fourthNumber').val()
     var ccv = $('#ccv').val()
-    var expirationDate =  $('#month').val()+$('#year').val()
+    var expirationDate =  $('#month').val()+ '/'+ $('#year').val()
     var brand = $('#brand').val()
 
     var crediCardNumber =  firstNumber+secondNumber+thirdNumber+fourthNumber
 
-    console.log(12345, crediCardNumber)
-
+    // console.log(totalPrice, crediCardNumber)
+    var data = {
+      machine_id: 1,
+      popsicles: [
+          { amount:parseInt($('#quantity0').val()), popsicle_id: 1 },
+          { amount:parseInt($('#quantity1').val()), popsicle_id: 2 },
+          { amount:parseInt($('#quantity2').val()), popsicle_id: 3 },
+          { amount:parseInt($('#quantity3').val()), popsicle_id: 4 }
+       ],
+       cielo_data: {
+           MerchantOrderId:'1091935197',
+           Payment: {
+               Type: "CreditCard",
+               Amount: '1000',
+               Installments: 1,
+               CreditCard: {
+                   CardNumber: crediCardNumber,
+                   ExpirationDate: expirationDate,
+                   SecurityCode: ccv,
+                   Brand: brand
+               }
+           }
+        }
+     }
     $.ajax({
-        dataType: "json",
-        url: "http://picole-pi2.herokuapp.com/purchases/",
-        data:{
-        "machine_id": 1,
-        "popsicles": [
-            { "amount":parseInt($('#quantity0').val()), "popsicle_id": 1 },
-            { "amount":parseInt($('#quantity1').val()), "popsicle_id": 2 },
-            { "amount":parseInt($('#quantity2').val()), "popsicle_id": 3 },
-            { "amount":parseInt($('#quantity3').val()), "popsicle_id": 4 }
-         ]
-        },
-        "cielo_data": {
-            "MerchantOrderId":"1091935197",
-            "Payment":{
-                "Type":"CreditCard",
-                "Amount":totalPrice,
-                "Installments":1,
-                "CreditCard":{
-                    "CardNumber":crediCardNumber,
-                    "ExpirationDate":expirationDate,
-                    "SecurityCode": ccv,
-                    "Brand": brand
-                }   
-            }
-         },
-         beforeSend: function(xhr){
-            xhr.setRequestHeader('Content-Type', 'application/json');
-        },
+      // url: "http://picole-pi2.herokuapp.com/purchases/",
+        url: "http://localhost:8000/purchases/",
+        data: JSON.stringify(data),
         type: "POST",
-        success: function(data) { alert('Sucesso!'+ data ); },
-        error: function(erro) { alert('Erro!'); 
-        console.log(erro);}
+        // processData: false,
+        traditional: true,
+        beforeSend: function(xhr){ xhr.setRequestHeader('Content-Type', 'application/json');},
+        success: function(data) { console.log(data); },
+        error: function(erro) { console.log(erro); }
     });
 }
-
