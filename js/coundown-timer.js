741dac6425350563620f372c0e45c6b2f855a756
Ludimila <lbelacruz@gmail.com>
2017-07-07 10:35:08 -0300

    Corrigindo bug do checkbuy e timer

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -1,16 +1,17 @@
 $(window).on('load',function(){
         document.getElementById("freeButton").style.display="block"
-        var checkBuy = localStorage.getItem('buy')
-         console.log(checkBuy)
-        if(!checkBuy){
+        // var checkBuy = localStorage.getItem('buy')
+        //  console.log('SOU '+checkBuy)
+        if(localStorage.getItem('buy')==='false'){
+            console.log('NAO SOU '+checkBuy)
             $('#checkBuy').modal('show');
             document.getElementById("freeButton").style.display="none"
-            // window.location.replace('https://picole.surge.sh/')
+            window.location.replace('https://picole.surge.sh/')
         }
     });
 
-
-// Set the date we're counting down to var date = new Date();
+// Set the date we're counting down to
+var date = new Date();
 var countDownDate = new Date();
 countDownDate.setHours(date.getHours() + 8 );
 
@@ -19,24 +20,24 @@ var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-
+    
     // Find the distance between now an the count down date
     var distance = countDownDate - now;
-
+    
     // Time calculations for days, hours, minutes and seconds
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+    
     // Output the result in an element with id="demo"
     document.getElementById("hour").innerHTML = hours
 
     // Output the result in an element with id="demo"
-    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("minutes").innerHTML = minutes 
 
 		// Output the result in an element with id="demo"
     document.getElementById("seconds").innerHTML =  seconds
-    // If the count down is over, write some text
+    // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("expire").innerHTML = "EXPIRADO";
